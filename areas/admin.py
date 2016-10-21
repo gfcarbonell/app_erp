8cdb4ebeb83d27cd22314de5a4b7fb5c036147d2
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2016-10-21 14:28:50 -0500

    Optimizando el perfil del empleado, modificar la parte de las ubicaciones y direcciones

--- areas/admin.py
+++ areas/admin.py
@@ -1,16 +1,11 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
-from .models import Area 
+from .models import Area
 from infos_sistemas.admin import InfoSistemaAdmin
 
 
 @admin.register(Area)
 class AreaAdmin(InfoSistemaAdmin):
-	list_display   = ( 'sede', 'area_dependiente', 'tipo_area_funcional', 'tipo_area', 'nombre', 'siglas', 'email', 'fax_interno',
-						'anexo_interno', 'mision', 'vision', 'logotipo', 'fecha_creacion', 'fecha_cese', 'descripcion', 'observacion',
-						'activo',
-					  'slug', 'fecha_registro', 'usuario_creador', 'nombre_host', 'direccion_ip',
-					  'fecha_ultima_actualizacion', 'ultimo_usuario_editor', 'ultimo_nombre_host', 'ultimo_direccion_ip')
 	list_instances = True
 	search_fields  = ('nombre',)
 
--- personas/models.py
+++ personas/models.py
@@ -5,9 +5,9 @@ from django.db import models
 from documentos_identificaciones.models import DocumentoIdentificacion
 from estados_civiles.models import EstadoCivil
 from grupos_sanguineos.models import GrupoSanguineo
-from distritos.models import Distrito 
-from vias.models import Via 
-from zonas.models import Zona 
+from distritos.models import Distrito
+from vias.models import Via
+from zonas.models import Zona
 from infos_sistemas.models import InfoSistema
 from ubicaciones.models import Ubicacion
 from .validators import valid_extension
@@ -20,7 +20,6 @@ from django.core.validators import MinValueValidator
 
 from django.core.validators import EmailValidator
 from django.core.validators import validate_ipv46_address
-
 from django.template.defaultfilters import slugify
 
 
@@ -30,50 +29,50 @@ class Persona(InfoSistema, Ubicacion):
 	CHOICES_TIPO_PERSONA 			= (('Natural', 'Natural'), ('Jurídica', 'Jurídica'))
 
 	tipo_persona 					= models.CharField(choices=CHOICES_TIPO_PERSONA, max_length=8, default=CHOICES_TIPO_PERSONA[0][1], help_text='Seleccionar tipo de persona.')
-	apellido_paterno 		 		= models.CharField(max_length=255, 
+	apellido_paterno 		 		= models.CharField(max_length=255,
 													   help_text='Escribir apellido paterno.',
 													   validators=[
 															        MinLengthValidator(1),
 															        MaxLengthValidator(255),
 															    ],
 													   db_index=True)
-	apellido_materno 		 		= models.CharField( max_length=255, 
+	apellido_materno 		 		= models.CharField( max_length=255,
 														validators=[
 															        MinLengthValidator(1),
 															        MaxLengthValidator(255),
 															    ],
-														help_text='Escribir apellido materno.', 
+														help_text='Escribir apellido materno.',
 														db_index=True)
-	nombre 			 		 		= models.CharField( verbose_name='Nombre(s)',	
-													    max_length=255, 
+	nombre 			 		 		= models.CharField( verbose_name='Nombre(s)',
+													    max_length=255,
 														validators=[
 															        MinLengthValidator(1),
 															        MaxLengthValidator(255),
 															    ],
-													    help_text='Escribir nombre(s).', 
+													    help_text='Escribir nombre(s).',
 													    db_index=True)
 	documento_identificacion 		= models.ForeignKey(DocumentoIdentificacion, verbose_name='Documento identificación',  related_name='%(app_label)s_%(class)s_related')
-	numero_documento_identificacion = models.CharField(verbose_name='Número documento identificación', 
-													   unique=True, 
-													   max_length=20, 
+	numero_documento_identificacion = models.CharField(verbose_name='Número documento identificación',
+													   unique=True,
+													   max_length=20,
 													   validators=[
 													   	        MinLengthValidator(1),
 													   	        MaxLengthValidator(20),
 													   	    ],
-													   help_text='Escribir número documento identificación.', 
+													   help_text='Escribir número documento identificación.',
 													   db_index=True)
 	fecha_nacimiento			    = models.DateField()
 	genero 							= models.CharField(verbose_name='Género', choices=CHOICES_GENERO, default=CHOICES_GENERO[0][1], max_length=9)
 	estado_civil 					= models.ForeignKey(EstadoCivil, related_name='%(app_label)s_%(class)s_related')
 	grupo_sanguineo 				= models.ForeignKey(GrupoSanguineo, verbose_name='Grupo sanguíneo', related_name='%(app_label)s_%(class)s_related')
-	fotografia   					= models.ImageField(blank=True, 
-														null=True, 
+	fotografia   					= models.ImageField(blank=True,
+														null=True,
 														verbose_name='Fotografía',
 														upload_to='fotografia_persona',
 														help_text='Subir fotografia (Opcional).',
 														validators=[valid_extension]
 														)
-	hijo							= models.BooleanField(verbose_name='¿Hijo(s)?', 
+	hijo							= models.BooleanField(verbose_name='¿Hijo(s)?',
 														  choices=BOOL_HIJO, default=False)
 	numero_hijo						= models.PositiveSmallIntegerField(
 																		blank=True,
@@ -84,36 +83,36 @@ class Persona(InfoSistema, Ubicacion):
 																			        MaxValueValidator(10),
 																			    ],
 																		help_text='Escribir número(s) de hijo(s).')
-	telefono 						= models.CharField(verbose_name="Télefono (Personal)", 
-													    max_length=20, 
+	telefono 						= models.CharField(verbose_name="Télefono (Personal)",
+													    max_length=20,
 													    blank=True, null=True, help_text='(Opcional).')
-	celular 						= models.CharField(verbose_name="Celular (Personal)", 
+	celular 						= models.CharField(verbose_name="Celular (Personal)",
 														max_length=20, blank=True, null=True, help_text='(Opcional).')
 	email 							= models.EmailField(
 														verbose_name="Correo electrónico (Personal)",
-														max_length=100, 
-														unique=True, 
-														db_index=True, 
+														max_length=100,
+														unique=True,
+														db_index=True,
 														validators=[
 																EmailValidator(),
 																MinLengthValidator(1),
-																MaxLengthValidator(100),	
+																MaxLengthValidator(100),
 															]
 														)
 	observacion_persona 			= models.TextField(verbose_name='Observación (Persona)',
-													   blank=True, null=True, 
+													   blank=True, null=True,
 													   help_text='Escribir observación de la persona (Opcional).')
 	distrito 						= models.ForeignKey(Distrito, related_name='%(app_label)s_%(class)s_related')
 	zona							= models.ForeignKey(Zona, related_name='%(app_label)s_%(class)s_related')
 	via								= models.ForeignKey(Via, related_name='%(app_label)s_%(class)s_related')
 	nombre_direccion				= models.CharField(
 														verbose_name='Nombre dirección',
-														max_length=255, 
+														max_length=255,
 														validators=[
 															        MinLengthValidator(1),
 															        MaxLengthValidator(255),
 															    ],
-														db_index=True, 
+														db_index=True,
 														help_text='Escribir nombre de la dirección.')
 	observacion_direccion 			= models.TextField( verbose_name='Observación (Dirección)',
 														blank=True, null=True, help_text='(Opcional).')
@@ -126,4 +125,3 @@ class Persona(InfoSistema, Ubicacion):
 
 	class Meta:
 		abstract = True
-
